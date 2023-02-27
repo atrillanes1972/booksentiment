e757bd0b567158cd513e45a5f3494c4ec611da9d
Augustus Trillanes <45346263+atrillanes1972@users.noreply.github.com>
2023-02-28 04:37:11 +0800

    Committing basic RE exercises in main, searched for Chapter headings in the book. Added the book itself.

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+import re
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+with open ("miracle_in_the_andes.txt", "r", encoding='utf-8') as file:
+    book = file.read()
 
+#print(book, end='')
+# without RE expressions
+print(book.count("Chapter"))
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# with RE
+pattern = re.compile("Chapter [0-9]+")
+findings = re.findall(pattern,book)
+print(findings)
